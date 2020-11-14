c99c178cf4d923d90aab48c8b3a3acd6720e1feb
ifeadikanwa <ezeifyyy@gmail.com>
2020-11-14 03:26:50 -0500

    Created custom dialog for inputting expense & income

--- app/src/main/java/com/example/expen/AddEntryDialog.java
+++ app/src/main/java/com/example/expen/AddEntryDialog.java
@@ -25,14 +25,22 @@ public class AddEntryDialog extends AppCompatDialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.add_entry_dialog, null);
+        builder.setView(view);
 
         addExpense = view.findViewById(R.id.addExpenseTextView);
         addIncome = view.findViewById(R.id.addIncomeTextView);
 
-        //go to choose category activity
-        addExpense.setOnClickListener(view1 -> startActivity(new Intent(getContext(), ExpenseCategoryActivity.class)));
+        //go to choose expense category activity
+        addExpense.setOnClickListener(view1 -> {
+            startActivity(new Intent(getContext(), ExpenseCategoryActivity.class));
+            dismiss();
+        });
 
-        addIncome.setOnClickListener(view2 -> startActivity(new Intent(getContext(), IncomeCategoryActivity.class)));
+        //go to choose income category activity
+        addIncome.setOnClickListener(view2 -> {
+            startActivity(new Intent(getContext(), IncomeCategoryActivity.class));
+            dismiss();
+        });
 
         return builder.create();
     }
