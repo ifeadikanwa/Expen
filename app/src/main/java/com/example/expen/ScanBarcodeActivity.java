6f6ce289113c8ac8b509822e354a94fe5c334fc8
SRM <nnguy22@u.rochester.edu>
2020-11-14 17:57:50 -0500

    fixed barcode?

--- app/src/main/java/com/example/expen/ScanBarcodeActivity.java
+++ app/src/main/java/com/example/expen/ScanBarcodeActivity.java
@@ -22,6 +22,7 @@ public class ScanBarcodeActivity extends AppCompatActivity  {
         scanBtn = findViewById(R.id.scan_button);
         formatTxt = findViewById(R.id.scan_format);
         contentTxt = findViewById(R.id.scan_content);
+        recyclableTxt = findViewById(R.id.recyclable);
         type = findViewById(R.id.type);
         scanBtn.setOnClickListener(this::onClick);
     }
@@ -36,27 +37,12 @@ public class ScanBarcodeActivity extends AppCompatActivity  {
         super.onActivityResult(requestCode, resultCode, intent);
         IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
         if (scanningResult != null) {
-
+            String scanContent = scanningResult.getContents();
             String scanFormat = scanningResult.getFormatName();
-            if (scanFormat == "UPC-A"){
-                String scanContent = scanningResult.getContents();
-                formatTxt.setText("Barcode Format: " + scanFormat);
-                contentTxt.setText("Barcode: " + scanContent);
-                recyclableTxt.setText("Recyclable");
-                type.setText("Type: Plastic0");
-            }
-            if else (scanFormat == "UPC-E")
-            {
-                String scanContent = scanningResult.getContents();
-                formatTxt.setText("Barcode Format: " + scanFormat);
-                contentTxt.setText("Barcode: " + scanContent);
-                recyclableTxt.setText("Recyclable");
-                type.setText("Type: Plastic0");
-            }
-            else {
-
-            }
-
+            formatTxt.setText("Barcode Format: " + scanFormat);
+            contentTxt.setText("Barcode: " + scanContent);
+            recyclableTxt.setText("Recyclable");
+            type.setText("Type: Plastic0");
 
         } else {
             Toast toast = Toast.makeText(getApplicationContext(),
