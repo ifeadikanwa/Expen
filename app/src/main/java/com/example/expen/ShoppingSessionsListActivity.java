afbe923ba67e24884888f861fdcc1fd6be4276d5
Jonnelin Marzielli Leonardo <jonnelin.leonardo@gmail.com>
2020-11-14 06:28:33 -0500

    Complete basic shopping sessions list ui

---
+++ app/src/main/java/com/example/expen/ShoppingSessionsListActivity.java
@@ -0,0 +1,14 @@
+package com.example.expen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShoppingSessionsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_sessions_list2);
+    }
+}
