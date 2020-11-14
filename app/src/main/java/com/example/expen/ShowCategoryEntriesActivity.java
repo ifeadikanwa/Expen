4731c37c812102711fba7adee243addfffaecf36
Jonnelin Marzielli Leonardo <jonnelin.leonardo@gmail.com>
2020-11-14 05:47:50 -0500

    Complete basic show category entries activity ui

---
+++ app/src/main/java/com/example/expen/ShowCategoryEntriesActivity.java
@@ -0,0 +1,14 @@
+package com.example.expen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowCategoryEntriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_category_entries);
+    }
+}
